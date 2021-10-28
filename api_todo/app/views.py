ca5ab0b035b3c5be2ce8b5d43e8099c574969bfb
Isabella Fagioli <isabella.fagioli@gmail.com>
2021-10-28 07:59:11 -0300

    get put delete

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
-from rest_framework.decorators import  api_view
+from rest_framework.decorators import api_view
 from app.models import Todo
 from app.serializers import TodoSerializer
-
 from app.serializers import TodoSerializer
 from rest_framework.response import Response 
 from rest_framework import status
@@ -21,4 +20,23 @@ def todo_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT', 'DELETE'])
 
+def todo_detail_change_and_delete(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
